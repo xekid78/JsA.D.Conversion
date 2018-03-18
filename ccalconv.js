240aa19f9dbfc1a81c4a9a65c8f427868ad50eaa
Makoto Tanabe <xekid78@outlook.com>
2018-03-18 15:47:00 +0900

    Add files via upload

--- ccalconv.js
+++ ccalconv.js
@@ -1,5 +1,10 @@
-for (var seireki = 1926; seireki <= 1935; seireki++) {
-    process.stdout.write("西暦" + seireki + "年は、");
-    var syowa = seireki - 1925;
-    console.log("昭和" + syowa + "年です。");
-}
+(function () {
+    'use strict';
+
+    for (var seireki = 1926; seireki <= 1935; seireki++) {
+        process.stdout.write("西暦" + seireki + "年は、");
+        var syowa = seireki - 1925;
+        console.log("昭和" + syowa + "年です。");
+    }
+
+})();
