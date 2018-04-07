b4ab348ff827aab7e6f785fedaea62c0727a1ad9
Makoto Tanabe <xekid78@outlook.com>
2018-04-07 21:39:55 +0900

    Add files via upload

--- ccalconv.js
+++ ccalconv.js
@@ -1,9 +1,10 @@
-(function() {
+(function () {
     'use strict';
 
+    var syowa;
     for (var seireki = 1926; seireki <= 1935; seireki++) {
         process.stdout.write("西暦" + seireki + "年は、");
-        var syowa = seireki - 1925;
+        syowa = seireki - 1925;
         console.log("昭和" + syowa + "年です。");
     }
 
